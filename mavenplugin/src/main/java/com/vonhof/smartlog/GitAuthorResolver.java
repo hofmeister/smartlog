fe4c3101afd7a07fcb928cf73e73bab0b07ecc10
Henrik Hofmeister <henrik@newdawn.dk>
2013-12-24 12:27:37 +0100

    Optimized for making less calls to git

--- mavenplugin/src/main/java/com/vonhof/smartlog/GitAuthorResolver.java
+++ mavenplugin/src/main/java/com/vonhof/smartlog/GitAuthorResolver.java
@@ -13,25 +13,27 @@ import java.io.File;
 public class GitAuthorResolver implements AuthorResolver {
 
     private final Git git;
+    private final String currentUser;
+    private final String basePath;
 
     public GitAuthorResolver(Repository repository) {
         git = new Git(repository);
+        currentUser = git.getRepository().getConfig().getString("user",null,"email");
+        basePath = git.getRepository().getDirectory().getParentFile().getAbsolutePath();
     }
 
     @Override
     public AuthorMap resolveAuthor(File file, String className) throws Exception {
         final AuthorMap authorMap = new AuthorMap(className, FileUtils.readLines(file).size());
 
-        String basePath = git.getRepository().getDirectory().getParentFile().getAbsolutePath();
+
         String filePath = file.getAbsolutePath();
-        String relPath = filePath.substring(basePath.length()+1);
+        String relPath = filePath.substring(basePath.length() + 1);
 
         BlameResult result = git.blame().setFilePath(relPath).call();
         if (result != null) {
             result.computeAll();
         }
-
-
 
         for(int line = 0;line < authorMap.getAuthors().length;line++) {
             String author = "";
@@ -49,7 +51,7 @@ public class GitAuthorResolver implements AuthorResolver {
 
             if (StringUtils.isEmpty(author)) {
                 //Uncomitted lines - fall back to config user
-                author = git.getRepository().getConfig().getString("user",null,"email");
+                author = currentUser;
             }
 
             authorMap.put(line, author);
